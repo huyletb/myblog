da5b00c5517a74a6eee4c3dcf1171680b073bdae
huyletb <huyletb@gmail.com>
2017-04-22 14:19:24 +0700

    Update categories.php

--- myblog/common/categories.php
+++ myblog/common/categories.php
@@ -5,14 +5,14 @@
 		private $_cateory_id = "category_id";
 		private $_category_name = "category_name";
 
-		public function getCategories(){
+		public function getCategories() {
 			$sql = "SELECT * 
 					FROM $this->_category";
 			$this->query($sql);
 			return $this->fetchAll();
 		}
 
-		public function getCategoryName_byID($id){
+		public function getCategoryName_byID($id) {
 			$sql = "SELECT $this->_category_name
 					FROM $this->_category 
 					WHERE $this->_cateory_id = '$id' ";
