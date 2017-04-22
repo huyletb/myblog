803bcd499d854115cc47ef32ba6cc76079c6cd36
huyletb <huyletb@gmail.com>
2017-04-22 14:24:55 +0700

    Update right.php

--- myblog/modules/right.php
+++ myblog/modules/right.php
@@ -1,14 +1,14 @@
 <div class="right">
 	<?php
-		if(isset($_GET['view'])){
+		if (isset($_GET['view'])) {
 			$view = $_GET['view'];
-		}else $view = "";
+		} else $view = "";
 
-		if($view == "detail_category"){
+		if ($view == "detail_category") {
 			include 'content/detail_category.php';;
-		}else if ($view == 'detail_article'){
+		} else if ($view == 'detail_article') {
 			include 'content/detail_article.php';
-		}else{
+		} else {
 			include 'content/all_articles.php';
 		}
 	?>
