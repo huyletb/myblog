d2c56735746d1b8d6cc0fdb772692e55b060685d
huyletb <huyletb@gmail.com>
2017-04-22 14:24:11 +0700

    Update users.php

--- myblog/common/users.php
+++ myblog/common/users.php
@@ -1,10 +1,10 @@
 <?php
-class Users extends Database{
+class Users extends Database {
 	private $_user = "tbl_users";
 	private $_user_name = "user_name";
 	private $_user_id = "user_id";
 
-	public function getUserName_byID($id){
+	public function getUserName_byID($id) {
 		$sql = "SELECT $this->_user_name
 				FROM $this->_user
 				WHERE $this->_user_id = '$id'";
